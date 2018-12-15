121b107184c4cc5d12c4d052e4c869fe16b3c827
SchruteFarms23 <romanmullady@gmail.com>
2018-12-15 17:27:22 -0500

    form

---
+++ src/components/OrderForm/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class OrderForm extends React.Component {
+  state = {
+    formId:"",
+    patientId:"",
+    firstName:"",
+    lastName:"",
+    phoneNum:"",
+    date:"",
+    rightEye:{sphere:"",cylinder:"",axis:"",prismBase:"",add:"",ht:"",pd:""},
+    leftEye:{sphere:"",cylinder:"",axis:"",prismBase:"",add:"",ht:"",pd:""},
+    
+  };
+
+  render(){
+    return(
+
+    )
+  }
+}
